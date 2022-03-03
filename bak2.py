218a377e2c3b4707cc99b413924a5f7dca7faa8b
qc <chaoqini@qq.com>
2022-03-03 22:52:54 +0800

    20220303_22:52

--- bak2.py
+++ bak2.py
@@ -16,52 +16,76 @@ from matplotlib import pyplot as plt
 #    def wb2db(self,wbfile):
 #        wb=openpyxl.load_workbook(wbfile)
 
+def g(x)->str:
+    for k,v in locals().items():
+        if v is x:
+            return k
+
+def pt(x)->None:
+#def pt(x,et=0):
+#    print("dir:\n",dir(x))
+#    print("type is:\n ",type(x))
+#    print("%s is:\n "%x,x)
+#    y=x
+#    print(locals())
+#    print(locals().items())
+    print(g(x))
+#    print(globals())
+#    print(x,"=",eval(x))
+
+#    if et==0 : exit(0)
+#    return x
+
+zzz=range(10)
+yyy="aa"
+#print("%s"%yyy)
+pt(zzz)
+#print(yyy)
+exit(0)
 
 
-
-## ==========
-x=0
 #wbfile='data1.xlsx'
 #wbfile='/home/qc/download/2201-06-PM_VA05A_HTDR_Result_500hrs_20220216.xlsm'
-wbfile='test.xlsm'
-wb1=openpyxl.load_workbook(wbfile)
-wb2=openpyxl.Workbook()
-
-wbn1=wb1.get_sheet_names()#获取sheet页
-wbs1=wb1.get_sheet_by_name(wbn1[-5])
-wbn2=wb2.get_sheet_names()
-wbs2=wb2.get_sheet_by_name(wbn2[0])
-
-maxr=wbs1.max_row    #最大行数
-maxc=wbs1.max_column  #最大列数
-
-
+#wbfile1='test1.xlsx'
+wbfile1='data1.xlsx'
+wb1=openpyxl.load_workbook(wbfile1)
+wbfile2='test2.xlsx'
+wb2=openpyxl.load_workbook(wbfile2)
+#wb2=openpyxl.Workbook()
+#wbn1=wb1.get_sheet_names()#获取sheet页
+#wbs1=wb1.get_sheet_by_name(wbn1[-5])
+#wbn2=wb2.get_sheet_names()
+#wbs2=wb2.get_sheet_by_name(wbn2[0])
+wb1s1=wb1.worksheets[2]
+wb2s1=wb2.worksheets[0]
+maxr=wb1s1.max_row    #最大行数
+maxc=wb1s1.max_column  #最大列数
+pt(maxr,1)
 #db1=[]
-db1=[["" for i in range(1+maxr)]for i in range(1+maxc)]
+db1=[['' for i in range(1+maxr)]for i in range(1+maxc)]
 #val1=''
-for c in range(1,1+maxc):
-	for r in range(1,1+maxr):
-		val = wbs1.cell(row=r,column=c).value
-#		if val is not None
-#        db1[c][r] = wbs1.cell(row=r, column=c).value
-#		db1[c][r] = [val if val is not None else ""]
-		db1[c][r] = (val if val is not None else '')
+for x in range(maxc):
+    for y in range(maxr):
+        c=x+1
+        r=y+1
+        val = wb1s1.cell(row=r,column=c).value
+        db1[c][r] = (val if val is not None else '')
 
+for c in range(3,maxc+1):
+    data=db1[c][6:1+maxr]
+    data=[i for i in data if i != '']
+#    mean=np.mean(data)
+#    db1[c].append(db1[c][48])
+    if data!=[] :
+        db1[c].append(np.mean(data))
+        db1[c].append(np.std(data))
+        db1[c].append(max(data))
+        db1[c].append(min(data))
+wb2s2=wb2.worksheets[1]
+#for c in range(maxc):
+for c in range(len(db1)):
+	for r in range(len(db1[c])):
+		wb2s2.cell(row=r+1,column=c+1).value=db1[c][r]
 
-for c in range(1,1+maxc):
-	for r in range(1,1+maxr):
-		wbs2.cell(row=r,column=c).value=db1[c][r]
-#		wbs2.cell(row=r,column=c).value=str(db1[c][r])
-#		wbs2.cell(row=r,column=c)=db1[c][r]
-
-#x=str(db1[1][1])
-x=db1[1][1]
-#x=db1[1]
-wbs2.cell(row=1,column=1).value=x
 wb2.save('test2.xlsx')
-
-print("dir x :\n ",dir(x))
-print("x type is:\n ",type(x))
-print("x is:\n ",x)
-exit(0)
 
--- wbacbi.py
+++ wbacbi.py
@@ -16,98 +16,78 @@ from matplotlib import pyplot as plt
 #    def wb2db(self,wbfile):
 #        wb=openpyxl.load_workbook(wbfile)
 
+#def g(x)->str:
+#    for k,v in locals().items():
+#        if v is x:
+#            return k
 
+def pt(x,display=1,fexit=0):
+    print("dir is:\n",dir(x))
+    print("type is:\n ",type(x))
+    if display==1: print("variable is:\n",x)
+    if fexit!=0: exit(0)
+    return x
 
-
-## ==========
-x=0
 #wbfile='data1.xlsx'
 #wbfile='/home/qc/download/2201-06-PM_VA05A_HTDR_Result_500hrs_20220216.xlsm'
-wbfile1='test.xlsm'
+#wbfile1='test1.xlsx'
+wbfile1='data1.xlsx'
 wb1=openpyxl.load_workbook(wbfile1)
 wbfile2='test2.xlsx'
 wb2=openpyxl.load_workbook(wbfile2)
 #wb2=openpyxl.Workbook()
-
 #wbn1=wb1.get_sheet_names()#获取sheet页
 #wbs1=wb1.get_sheet_by_name(wbn1[-5])
 #wbn2=wb2.get_sheet_names()
 #wbs2=wb2.get_sheet_by_name(wbn2[0])
-
-wb1s1=wb1.worksheets[0]
+wb1s1=wb1.worksheets[2]
 wb2s1=wb2.worksheets[0]
-maxr=wb2s1.max_row    #最大行数
-maxc=wb2s1.max_column  #最大列数
-
-
+maxr=wb1s1.max_row    #最大行数
+maxc=wb1s1.max_column  #最大列数
+#pt(maxr)
+#pt(maxc)
+#pt(wb1s1.title)
+#x=wb1s1.cell(row=1,column=1).value
 #db1=[]
-db1=[["" for i in range(1+maxr)]for i in range(1+maxc)]
+db1=[['' for i in range(1+maxr)]for i in range(1+maxc)]
 #val1=''
-for c in range(1,maxc+1):
-	for r in range(1,maxr+1):
-#		val = wbs1.cell(row=r,column=c).value
-		val = wb2s1.cell(row=r,column=c).value
-		db1[c][r] = (val if val is not None else '')
+for x in range(maxc):
+#for x in range(1):
+    for y in range(maxr):
+        val = wb1s1.cell(row=y+1,column=x+1).value
+        db1[x+1][y+1] = (val if val is not None else '')
 
+#pt(db1)
 
-#for c in range(7,8):
-for c in range(7,maxc+1):
-#for c in range(84,86+1):
-    data=db1[c][53:1+92]
+#with open('1.txt','w') as f:
+#    for dc in db1:
+#        f.write('\n')
+#    for v in dc:
+#            if v != '' :
+##                print(v,"\r")
+#                f.write(str(v)+' ')
+
+#pt(len(db1))
+#pt(len(db1[0]))
+for c in range(3,maxc+1):
+    data=db1[c][6:maxr+1]
     data=[i for i in data if i != '']
 #    mean=np.mean(data)
-    db1[c].append(db1[c][48])
+#    db1[c].append(db1[c][48])
     if data!=[] :
         db1[c].append(np.mean(data))
         db1[c].append(np.std(data))
         db1[c].append(max(data))
         db1[c].append(min(data))
 
+#pt(len(db1))
+#pt(len(db1[4]))
 
-#wb2s2=wb2.create_sheet("ratio",1)
 wb2s2=wb2.worksheets[1]
-maxc = len(db1)
-maxr = len(db1[10])
-#for c in range(maxc):
-for c in range(10):
-#	for r in range(maxr):
-	for r in range(maxr):
-		wb2s2.cell(row=r+1,column=c+1).value=db1[c][r]
-
+i=0
+for x in range(len(db1)):
+    for y in range(len(db1[x])):
+        wb2s2.cell(column=x+1,row=y+1).value=db1[x][y]
+#pt(i)
+#pt(wb2s2.title)
 wb2.save('test2.xlsx')
-#x=db1[2][53:1+92]
-#x=max(x)
-#x=min(x)
-#x=np.mean(x)
-#x=np.std(x)
-#x=format(x,'0.2f')
-#db1[2].append("xxxxxxx")
-#db1[2].append({"mean":123})
-#db1[2].append(db1[2][48])
-#x=db1[2]
-#x=len(db1)
-x=len(db1[0])
-#x=db1
-
-print("dir x :\n ",dir(x))
-print("x type is:\n ",type(x))
-print("x is:\n ",x)
-
-
-exit(0)
-
-for c in range(1,1+maxc):
-	for r in range(1,1+maxr):
-		wbs2.cell(row=r,column=c).value=db1[c][r]
-
-#x=str(db1[1][1])
-x=db1[1][1]
-#x=db1[1]
-wbs2.cell(row=1,column=1).value=x
-wb2.save('test2.xlsx')
-
-print("dir x :\n ",dir(x))
-print("x type is:\n ",type(x))
-print("x is:\n ",x)
-exit(0)
-
