1be2c558bbac2ba5944ebcaab1b53cf683f09966
qc <chaoqini@qq.com>
2022-03-22 00:02:10 +0800

    20220322_00:02

--- mnist.py
+++ mnist.py
@@ -1,8 +1,8 @@
 
-import pandas as pd
+import numpy as np
+#import pandas as pd
 from pathlib import Path
 import struct
-import numpy as np
 import matplotlib.pyplot as plt
 
 def pt(x,display=1,fexit=0):
@@ -13,21 +13,31 @@ def pt(x,display=1,fexit=0):
     return x
 
 mnist_path=Path('./mnist')
-#pt(mnist_path)
 train_img_path=mnist_path/'train-images-idx3-ubyte'
 train_lab_path=mnist_path/'train-labels-idx1-ubyte'
 test_img_path=mnist_path/'t10k-images-idx3-ubyte'
 test_lab_path=mnist_path/'t10k-labels-idx1-ubyte'
-#pt(train_img_path)
 
-#f=open(train_img_path,'rb')
+train_num=50000
+valid_num=10000
+test_num=10000
+
 with open(train_img_path,'rb') as f:
     struct.unpack('>4i',f.read(16))
-    a=np.fromfile(f,dtype=np.uint8)
-    train_img=a.reshape(-1,28*28)
-#pt(a)
-#pt(a.ndim)
-#pt(a.shape)
+    tmp_img=np.fromfile(f,dtype=np.uint8).reshape(-1,28*28)
+    train_img=tmp_img[:train_num]
+    valid_img=tmp_img[train_num:]
+with open(test_img_path,'rb') as f:
+    struct.unpack('>4i',f.read(16))
+    test_img=np.fromfile(f,dtype=np.uint8).reshape(-1,28*28)
+with open(train_lab_path,'rb') as f:
+    struct.unpack('>2i',f.read(8))
+    tmp_lab=np.fromfile(f,dtype=np.uint8)
+    train_lab=tmp_lab[:train_num]
+    valid_lab=tmp_lab[train_num:]
+with open(test_lab_path,'rb') as f:
+    struct.unpack('>2i',f.read(8))
+    test_lab=np.fromfile(f,dtype=np.uint8)
 
 n=np.random.randint(50000)
 img=train_img[n].reshape(28,28)
